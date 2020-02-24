11631261120c87a89faa58122a50dfc6fe015653
Vanya <vanya.gordejchik@mail.ru>
2020-02-24 17:22:55 +0300

    task9

--- task9.cpp
+++ task9.cpp
@@ -1,11 +1,16 @@
-
+//A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+//
+//	a2 + b2 = c2
+//	For example, 32 + 42 = 9 + 16 = 25 = 52.
+//
+//	There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+//	Find the product abc.
 #include<iostream>
 #include<cmath>
 using namespace std;
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	cout<<"Тройка Пифагора - три натуральных числа a < b < c, для которых выполняется равенство a2 + b2 = c2		Существует только одна тройка Пифагора, для которой a + b + c = 1000.		Найдите произведение abc.";
+	
 	for (size_t a = 0; a < 1000; a++)
 	{
 		for (size_t b = a+1; b < 1000; b++)
